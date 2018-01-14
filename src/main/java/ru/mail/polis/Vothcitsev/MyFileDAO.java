3337222021ae4d60c8ac627ec687dfb987d25cea
lotos580 <vothcicev_k@mail.ru>
2018-01-14 20:40:51 +0300

    inline getPath method

--- src/main/java/ru/mail/polis/Vothcitsev/MyFileDAO.java
+++ src/main/java/ru/mail/polis/Vothcitsev/MyFileDAO.java
@@ -6,15 +6,12 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.NoSuchElementException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MyFileDAO implements MyDAO {
     @NotNull
     private final File dir;
-
-    @NotNull
-    private File getFile(@NotNull final String key){
-        return new File(dir, key);
-    }
 
     public MyFileDAO(@NotNull final File dir) {
         this.dir = dir;
@@ -28,7 +25,7 @@ public class MyFileDAO implements MyDAO {
 
     @Override
     public void upsert(@NotNull final String key, @NotNull final byte[] value)throws IllegalArgumentException, IOException{
-        try(OutputStream os = new FileOutputStream(getFile(key))){
+        try(OutputStream os = new FileOutputStream(new File(dir, key))){
             os.write(value);
         } catch (IOException e) {
             e.printStackTrace();
@@ -37,6 +34,6 @@ public class MyFileDAO implements MyDAO {
 
     @Override
     public void delete(@NotNull final String key) throws IllegalArgumentException, IOException{
-        getFile(key).delete();
+        new File(dir, key).delete();
     }
 }
